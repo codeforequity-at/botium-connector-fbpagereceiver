0791e23373464de5cabb80b47cab1c30a81ea39e
treml <ftreml@gmail.com>
2019-01-03 17:11:53 +0100

    Add 2-factor-auth (optionally)

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# botium-connector-fbpagereceiver
-Botium Connector for Facebook Messenger Bots
+# Botium Connector for Facebook Messenger Bots 
+
+[![NPM](https://nodei.co/npm/botium-connector-fbpagereceiver.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/botium-connector-fbpagereceiver/)
+
+[ ![Codeship Status for codeforequity-at/botium-connector-dialogflow](https://app.codeship.com/projects/1c935480-633f-0136-f02a-52b5f01093c8/status?branch=master)](https://app.codeship.com/projects/296958)
+[![npm version](https://badge.fury.io/js/botium-connector-fbpagereceiver.svg)](https://badge.fury.io/js/botium-connector-fbpagereceiver)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
+
+This is a [Botium](https://github.com/codeforequity-at/botium-core) connector for testing your Facebook Messenger chatbot.
+
+__Did you read the [Botium in a Nutshell](https://medium.com/@floriantreml/botium-in-a-nutshell-part-1-overview-f8d0ceaf8fb4) articles ? Be warned, without prior knowledge of Botium you won't be able to properly use this library!__
+
+## How it works ?
+Botium runs your conversations against the Dialogflow API.
+
+It can be used as any other Botium connector with all Botium Stack components:
+* [Botium CLI](https://github.com/codeforequity-at/botium-cli/)
+* [Botium Bindings](https://github.com/codeforequity-at/botium-bindings/)
+* [Botium Box](https://www.botium.at)
+
+## Requirements
+
+* __Node.js and NPM__
+* a __Facebook Messenger__ chatbot, and a facebook developer account with administrative rights
+* a __project directory__ on your workstation to hold test cases and Botium configuration
+
+## Install Secondary Receiver Webhook
+
+Events to subscribe to: messages, messaging_postbacks, message_echoes, standby
+
+
+
+## Install Botium and Dialogflow Connector
+
+When using __Botium CLI__:
+
+```
+> npm install -g botium-cli
+> npm install -g botium-connector-fbpagereceiver
+> botium-cli init
+> botium-cli run
+```
+
+When using __Botium Bindings__:
+
+```
+> npm install -g botium-bindings
+> npm install -g botium-connector-fbpagereceiver
+> botium-bindings init mocha
+> npm install && npm run mocha
+```
+
+When using __Botium Box__:
+
+_Already integrated into Botium Box, no setup required_
+
+## Connecting Dialogflow Agent to Botium
+
+Open the file _botium.json_ in your working directory and add the Google credentials for accessing your Dialogflow agent. [This article](https://chatbotsmagazine.com/3-steps-setup-automated-testing-for-google-assistant-and-dialogflow-de42937e57c6) shows how to retrieve all those settings.
+
+```
+{
+  "botium": {
+    "Capabilities": {
+      "PROJECTNAME": "<whatever>",
+      "CONTAINERMODE": "dialogflow",
+      "DIALOGFLOW_PROJECT_ID": "<google project id>",
+      "DIALOGFLOW_CLIENT_EMAIL": "<service credentials email>",
+      "DIALOGFLOW_PRIVATE_KEY": "<service credentials private key>"
+    }
+  }
+}
+```
+
+To check the configuration, run the emulator (Botium CLI required) to bring up a chat interface in your terminal window:
+
+```
+> botium-cli emulator
+```
+
+Botium setup is ready, you can begin to write your [BotiumScript](https://github.com/codeforequity-at/botium-core/wiki/Botium-Scripting) files.
+
+## Supported Capabilities
+
+Set the capability __CONTAINERMODE__ to __fbpagereceiver__ to activate this connector.
+
+### FBPAGERECEIVER_PAGEID
+### FBPAGERECEIVER_TESTUSER
+### FBPAGERECEIVER_TESTPASSWORD
+### FBPAGERECEIVER_TESTUSER_FULLNAME
+### FBPAGERECEIVER_ASKFORLOGINAPPROVAL
+### FBPAGERECEIVER_REDISURL
+
+## Open Issues and Restrictions
+
+* Quick Reply buttons are not delivered to the Secondary Receiver webhook, so Botium won't receive it. It will receive cards and pictures though.
--- src/connector.js
+++ src/connector.js
@@ -1,6 +1,7 @@
 const util = require('util')
 const Redis = require('ioredis')
 const fblogin = require('facebook-chat-api')
+const readline = require('readline')
 const _ = require('lodash')
 const debug = require('debug')('botium-connector-fbpagereceiver')
 const debugFbApi = require('debug')('botium-connector-fbpagereceiver-FbApi')
@@ -10,6 +11,7 @@ const Capabilities = {
   FBPAGERECEIVER_TESTUSER: 'FBPAGERECEIVER_TESTUSER',
   FBPAGERECEIVER_TESTPASSWORD: 'FBPAGERECEIVER_TESTPASSWORD',
   FBPAGERECEIVER_TESTUSER_FULLNAME: 'FBPAGERECEIVER_TESTUSER_FULLNAME',
+  FBPAGERECEIVER_ASKFORLOGINAPPROVAL: 'FBPAGERECEIVER_ASKFORLOGINAPPROVAL',
   FBPAGERECEIVER_REDISURL: 'FBPAGERECEIVER_REDISURL'
 }
 
@@ -42,7 +44,6 @@ class BotiumConnectorFbPageReceiver {
 
   async Start () {
     debug('Start called')
-    await this._startFacebookApi()
     this.queueReceivedMessages = true
   }
 
@@ -56,8 +57,7 @@ class BotiumConnectorFbPageReceiver {
 
   async Stop () {
     debug('Stop called')
-
-    await this._stopFacebookApi()
+    this.queueReceivedMessages = false
   }
 
   async Clean () {
@@ -80,25 +80,59 @@ class BotiumConnectorFbPageReceiver {
           resolve()
         })
       })
-      this.redis.on('message', (channel, message) => {
+      this.redis.on('message', (channel, event) => {
         if (this.queueReceivedMessages) {
-          if (!_.isString(message)) {
-            return debug(`WARNING: received non-string message from ${channel}, ignoring: ${message}`)
+          if (!_.isString(event)) {
+            return debug(`WARNING: received non-string message from ${channel}, ignoring: ${event}`)
           }
           try {
-            message = JSON.parse(message)
+            event = JSON.parse(event)
           } catch (err) {
-            return debug(`WARNING: received non-json message from ${channel}, ignoring: ${message}`)
+            return debug(`WARNING: received non-json message from ${channel}, ignoring: ${event}`)
+          }
+          if (!event.message) {
+            return debug(`Received a message without content from my page at ${channel}: ${util.inspect(event)}`)
+          }
+          if (!event.sender.id || parseInt(event.sender.id) !== parseInt(this.caps[Capabilities.FBPAGERECEIVER_PAGEID])) {
+            return debug(`Received a message not originiating from my page at ${channel}: ${util.inspect(event)}`)
+          }
+          if (!event.recipient.id || event.recipient.name !== this.caps[Capabilities.FBPAGERECEIVER_TESTUSER_FULLNAME]) {
+            return debug(`Received a message not targeted at current user "${this.caps[Capabilities.FBPAGERECEIVER_TESTUSER_FULLNAME]}" at ${channel}: ${util.inspect(event)}`)
           }
 
-          if (!message.sender.id || message.sender.id !== this.caps[Capabilities.FBPAGERECEIVER_PAGEID]) {
-            return debug(`Received a message not originiating from my page at ${channel}: ${util.inspect(message)}`)
+          const botMsg = { sender: 'bot', sourceData: event }
+          if (event.message.text) {
+            botMsg.messageText = event.message.text
           }
-          if (!message.recipient.id || message.recipient.name !== this.caps[Capabilities.FBPAGERECEIVER_TESTUSER_FULLNAME]) {
-            return debug(`Received a message not targeted at current user "${this.caps[Capabilities.FBPAGERECEIVER_TESTUSER_FULLNAME]}" at ${channel}: ${util.inspect(message)}`)
+          if (event.message.attachments && event.message.attachments.length > 0) {
+            event.message.attachments.forEach(a => {
+              if (a.type === 'image' && a.payload && a.payload.url) {
+                botMsg.media = botMsg.media || []
+                botMsg.media.push({
+                  mediaUri: a.payload.url
+                })
+              }
+              if (a.type === 'template' && a.payload && a.payload.template_type === 'generic') {
+                a.payload.elements && a.payload.elements.forEach(e => {
+                  botMsg.cards = botMsg.cards || []
+                  const card = {
+                    text: e.title
+                  }
+                  if (e.image_url) {
+                    card.image = { mediaUri: e.image_url }
+                  }
+                  if (e.buttons) {
+                    card.buttons = e.buttons.map(b => ({
+                      text: b.title,
+                      payload: b.payload
+                    }))
+                  }
+                  botMsg.cards.push(card)
+                })
+              }
+            })
           }
 
-          const botMsg = { sender: 'bot', sourceData: message, messageText: message.text }
           debug(`Received a message to queue at ${channel}: ${util.inspect(botMsg)}`)
           this.queueBotSays(botMsg)
         } else {
@@ -120,39 +154,27 @@ class BotiumConnectorFbPageReceiver {
       debug(`logging into facebook page ${this.caps[Capabilities.FBPAGERECEIVER_PAGEID]} with user ${this.caps[Capabilities.FBPAGERECEIVER_TESTUSER]}`)
       fblogin({ email: this.caps[Capabilities.FBPAGERECEIVER_TESTUSER], password: this.caps[Capabilities.FBPAGERECEIVER_TESTPASSWORD] }, { logLevel: debugFbApi.enabled ? 'verbose' : 'warn' }, (err, api) => {
         if (err) {
-          return reject(new Error(`Facebook login failed: ${util.inspect(err)}`))
+          if (err.error === 'login-approval' && this.caps[Capabilities.FBPAGERECEIVER_ASKFORLOGINAPPROVAL]) {
+            const rl = readline.createInterface({
+              input: process.stdin,
+              output: process.stdout
+            })
+            rl.question('Please enter two-factor authentication code or authorise login from another browser: ', (answer) => {
+              err.continue(answer)
+            })
+          } else {
+            reject(new Error(`Facebook login failed: ${util.inspect(err)}`))
+          }
+        } else {
+          debug('logging into facebook ready')
+          this.fbapi = api
+          resolve()
         }
-        debug('logging into facebook ready')
-        this.fbapi = api
-        resolve()
       })
     })
   }
 
-  _startFacebookApi () {
-    this._stopFacebookApi()
-
-    this.fbapiStopListener = this.fbapi.listen((err, event) => {
-      if (err) {
-        debug(`fbapi Error: ${util.inspect(err)}`)
-      } else if (event.type === 'message') {
-        debug(`fbapi received message: ${util.inspect(event)}`)
-      } else {
-        debug(`fbapi received ignored event: ${util.inspect(event)}`)
-      }
-    })
-  }
-
-  _stopFacebookApi () {
-    if (this.fbapiStopListener) {
-      this.fbapiStopListener()
-      this.fbapiStopListener = null
-    }
-  }
-
   _cleanFacebookApi () {
-    this._stopFacebookApi()
-
     if (this.fbApi) {
       return new Promise((resolve) => {
         this.fbapi.logout((err) => {
